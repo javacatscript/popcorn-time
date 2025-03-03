bf08ff6769da3a0ff5ee6053461696ba2ca02b2d
javacatscript <ru96nk@gmail.com>
2025-03-03 15:50:47 +0530

    setup dark theme

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,7 +1,20 @@
 import React from "react";
+import { fetchTrendingMovies } from "@/lib/tmdb";
 
-const MoviesPage = () => {
-  return <div>Movies Page</div>;
+const MoviesPage = async () => {
+  const data = await fetchTrendingMovies();
+  console.log(data);
+
+  return (
+    <div>
+      <h1>Movies of the week!</h1>
+      <ul>
+        {data.results.map((movie) => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
 };
 
 export default MoviesPage;
