b05f9113d318e3ae3ad8be8db0949a72621cfce3
javacatscript <ru96nk@gmail.com>
2025-03-03 14:20:24 +0530

    setup placeholder routes

--- src/app/movies/page.js
+++ src/app/movies/page.js
@@ -1,21 +1,7 @@
+import React from "react";
 
-export default function MoviesPage() {
-  const movies = [
-    { id: 1, title: "Inception" },
-    { id: 2, title: "The Matrix" },
-    { id: 3, title: "Interstellar" },
-  ];
+const MoviesPage = () => {
+  return <div>Movies Page</div>;
+};
 
-  return (
-    <div className="min-h-screen bg-black text-white p-6">
-      <h1 className="text-4xl text-electric-blue font-bold mb-6">All Movies</h1>
-      <ul className="space-y-4">
-        {movies.map((movie) => (
-          <li key={movie.id} className="text-xl">
-            {movie.title}
-          </li>
-        ))}
-      </ul>
-    </div>
-  );
-}
+export default MoviesPage;
